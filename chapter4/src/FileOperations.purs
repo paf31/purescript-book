69bd8bb54650b95e18ca43e44a5705e050306caf
Phil Freeman <paf31@cantab.net>
2014-08-01 19:04:19 -0700

    allFiles'

--- chapter4/src/FileOperations.purs
+++ chapter4/src/FileOperations.purs
@@ -5,3 +5,8 @@ import Data.Array
 
 allFiles :: Path -> [Path]
 allFiles root = root : concatMap allFiles (ls root)
+
+allFiles' :: Path -> [Path]
+allFiles' file = file : do
+  child <- ls file
+  allFiles' child
