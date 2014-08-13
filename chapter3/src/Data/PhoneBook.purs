074c37228defa2fde8412e758a89e75f3035dc3f
Phil Freeman <paf31@cantab.net>
2014-08-13 12:15:46 -0700

    Make import list explicit

--- chapter3/src/Data/PhoneBook.purs
+++ chapter3/src/Data/PhoneBook.purs
@@ -3,7 +3,7 @@ module Data.PhoneBook where
 import Data.List
 import Data.Maybe
 
-import Control.Plus
+import Control.Plus (empty)
 
 type Entry = { firstName :: String, lastName :: String, phone :: String }
 
