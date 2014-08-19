4556e1bb742c3046d67ef64623411e1e2086335f
Phil Freeman <paf31@cantab.net>
2014-08-18 17:56:48 -0700

    Chapter 10 empty project

---
+++ chapter10/Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  "use strict";
+
+  grunt.initConfig({
+
+    srcFiles: [
+      "src/**/*.purs", 
+      "bower_components/**/src/**/*.purs",
+    ],
+
+    psc: {
+      options: {
+        modules: ["Main"]
+      },
+      all: {
+	src: ["<%=srcFiles%>"],
+        dest: "dist/Main.js"
+      }
+    },
+
+    dotPsci: ["<%=srcFiles%>"]
+  });
+
+  grunt.loadNpmTasks("grunt-purescript");
+  grunt.registerTask("default", ["psc:all", "dotPsci"]);
+};
---
+++ chapter10/README.md
@@ -0,0 +1,11 @@
+# purescript-book-chapter7
+
+Applicative Validation
+
+## Building
+
+```
+npm install
+bower update
+grunt
+```
