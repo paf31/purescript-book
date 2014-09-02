68aa3d253c85b207cdede306ac8d82e7d63f6eb7
Phil Freeman <paf31@cantab.net>
2014-09-01 18:23:24 -0700

    Implement foldC

--- chapter12/src/Control/Monad/Cont/Extras.purs
+++ chapter12/src/Control/Monad/Cont/Extras.purs
@@ -2,7 +2,9 @@ module Control.Monad.Cont.Extras where
 
 import Data.Array ()
 import Data.Maybe
+import Data.Tuple
 
+import Control.Monad
 import Control.Monad.Eff
 import Control.Monad.Eff.Ref
 
@@ -11,18 +13,25 @@ import Control.Monad.Cont.Trans
 
 type WithRef eff = Eff (ref :: Ref | eff)
 
-collect :: forall eff a. ContT Unit (WithRef eff) (Maybe a) -> 
-                         ContT Unit (WithRef eff) [a]
-collect c = do
-  r <- lift $ newRef []
+type ContRef eff = ContT Unit (WithRef eff)
+
+foldC :: forall eff a r. (r -> a -> Tuple Boolean r) -> r -> ContRef eff a -> ContRef eff r
+foldC f r0 c = do
+  current <- lift $ newRef r0
   callCC $ \k -> do
-    m <- c
-    xs <- lift $ readRef r
-    case m of
-      Nothing -> k xs
-      Just x -> quietly $ lift $ writeRef r (xs ++ [x])
+    a <- c
+    r <- lift $ readRef current
+    case f r a of
+      Tuple emit next -> do
+        when emit $ k next 
+        quietly $ lift $ writeRef current next
 
   where
   quietly :: forall m a. (Monad m) => ContT Unit m Unit -> ContT Unit m a
   quietly = withContT (\_ _ -> return unit)
 
+collect :: forall eff a. ContRef eff (Maybe a) -> ContRef eff [a]
+collect = foldC f []
+  where
+  f xs Nothing = Tuple true xs
+  f xs (Just x) = Tuple false (xs ++ [x])
