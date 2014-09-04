303677f24dcbb87b08274973de2c5ba9643556ea
Phil Freeman <paf31@cantab.net>
2014-09-03 22:00:38 -0700

    Simplify example

--- chapter12/src/Main.purs
+++ chapter12/src/Main.purs
@@ -1,33 +1,17 @@
 module Main where
 
-import Data.Maybe
 import Data.Array (map)
-import Data.Function (on)
 import Data.String (joinWith, length)
 
 import Control.Monad.Eff
-import Control.Monad.Eff.Ref
-
-import Control.Monad.Parallel
 import Control.Monad.Cont.Trans
 
 import Network.HTTP.Client
 
 import Debug.Trace
 
-main = do
-  flip runContT k do
-    runParallel $ 
-      longest <$> Parallel (getResponseText purescript_org)
-              <*> Parallel (getResponseText try_purescript_org)
+main = runContT (getResponseText purescript_org) trace
   where
-  longest :: String -> String -> Ordering
-  longest = compare `on` length
-
-  k LT = trace "try.purescript.org"
-  k GT = trace "www.purescript.org"
-  k EQ = trace "Lengths are equal"
-
   getResponseText req = responseToString <$> getAll req
 
   responseToString :: Response -> String
@@ -37,10 +21,4 @@ main = do
   purescript_org = Request 
     { host: "www.purescript.org"
     , path: "/" 
-    }
-
-  try_purescript_org :: Request
-  try_purescript_org = Request
-    { host: "try.purescript.org"
-    , path: "/"
     }
