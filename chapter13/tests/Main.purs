4885bd03c877a0b0df8f98d805f95d801e6a7ecb
Phil Freeman <paf31@cantab.net>
2014-09-07 14:17:23 -0700

    Sorted newtype

--- chapter13/tests/Main.purs
+++ chapter13/tests/Main.purs
@@ -17,6 +17,14 @@ isSorted _ = true
 isSubarrayOf :: forall a. (Eq a) => [a] -> [a] -> Boolean
 isSubarrayOf xs ys = ys `intersect` xs == xs
 
+newtype Sorted a = Sorted [a]
+
+sorted :: forall a. Sorted a -> [a]
+sorted (Sorted xs) = xs
+
+instance arbSorted :: (Arbitrary a, Ord a) => Arbitrary (Sorted a) where
+  arbitrary = Sorted <<< sort <$> arbitrary
+
 numbers :: [Number] -> [Number]
 numbers = id
 
@@ -24,15 +32,15 @@ numberToBool :: (Number -> Boolean) -> Number -> Boolean
 numberToBool = id
 
 instance arbTree :: (Arbitrary a, Ord a) => Arbitrary (Tree a) where
-  arbitrary = fromArray <<< sort <$> arbitrary
+  arbitrary = fromArray <<< sorted <$> arbitrary
 
 main = do
   -- Tests for module 'Merge'
 
-  quickCheck $ \xs ys -> isSorted $ merge (sort xs) (sort ys)
+  quickCheck $ \xs ys -> isSorted $ merge (sorted xs) (sorted ys)
   quickCheck $ \xs ys -> xs `isSubarrayOf` merge xs ys
 
-  quickCheck $ \xs ys -> isSorted $ numbers $ mergePoly (sort xs) (sort ys)
+  quickCheck $ \xs ys -> isSorted $ numbers $ mergePoly (sorted xs) (sorted ys)
   quickCheck $ \xs ys -> numbers xs `isSubarrayOf` mergePoly xs ys
   
   quickCheck $ \xs ys f -> isSorted $ map f $ mergeWith (numberToBool f) (sortBy (compare `on` f) xs) (sortBy (compare `on` f) ys)
